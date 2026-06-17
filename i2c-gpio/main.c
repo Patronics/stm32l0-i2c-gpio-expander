4607ad1da9cf57fa734027375b79de1ea68e8eb8
Patrick Leiser <patrick27leiser@yahoo.com>
2026-06-16 20:38:49 -0700

    proof of toolchain successful, led blink upload works

--- i2c-gpio/main.c
+++ i2c-gpio/main.c
@@ -1,9 +1,27 @@
 #include "api.h"
 #include "api-asm.h"
 
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define LED GPIOA, GPIO7
+#define LITTLE_BIT 100000
+
 int main(void) {
 	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7);
+	gpio_set(LED);
+	
+	while(1) {
+		/* wait a little bit */
+		for (int i = 0; i < LITTLE_BIT; i++) {
+			__asm__("nop");
+		}
+		gpio_toggle(LED);
+	}
+	//uint32_t rev = 0xaabbccdd;
+	//rev = rev_bytes(rev);
+	//return my_func(rev);
 }
