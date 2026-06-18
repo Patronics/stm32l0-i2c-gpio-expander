bf9bd5991d61d9f8d0e74786687e965fb3840a8a
Patrick Leiser <patrick27leiser@yahoo.com>
2026-06-18 03:32:41 -0700

    fully implement i2c GPIO expander, now using PA4 through PA7 as inputs (pulled up by default), and PC14 through PC15 as outputs, along with an update indicator toggle on PB1

--- i2c-gpio/main.c
+++ i2c-gpio/main.c
@@ -8,16 +8,33 @@
 
 #include <stdbool.h>
 
-#define LED GPIOA, GPIO7
+//generic status LED on PB1, currently blinks when i2c communication received
+#define LED_PORT GPIOB
+#define LED_PIN GPIO1
 
 //uses pins PB6 (SCL) and PB7 (SDA) for i2c
+
+//inputs: PA4 through PA7
+#define IN_PORT GPIOA
+#define IN_PINS (GPIO4 | GPIO5 | GPIO6 | GPIO7)
+#define IN_PORT_SHIFT 4            //shift pin register down to pack into lower bits
+#define IN_MASK 0x0F               //4 input bits
+
+//outputs: PC14 and PC15
+#define OUT_PORT GPIOC
+#define OUT_PINS (GPIO14 | GPIO15)
+#define OUT_PORT_SHIFT 14        //shift low bits up to match pin positions
+#define OUT_MASK 0x03            //2 output bits
 
 
 #define BLINK_DELAY 10000
 #define I2C_ADDRESS 37
 
-volatile uint8_t inbyte = 0;
-volatile bool byte_received = false;
+volatile uint8_t inbyte = 0;   //input GPIO states, sampled continuously and sent on i2c read
+volatile uint8_t outbyte = 0;  //output GPIO states received from master on i2c write
+volatile bool is_read_transfer = false; //status of current ongoing i2c transfer
+volatile bool new_read = false;    //flag when new read to master completes, currently unused
+volatile bool new_write = false;   //flag when new write from master completes
 
 static void clock_setup(void){
 
@@ -30,12 +47,24 @@ static void clock_setup(void){
 }
 
 static void gpio_setup(void){
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7);
-	gpio_set(LED);
+
+	//generic status LED
+	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+	gpio_set(LED_PORT, LED_PIN);
+	
+	//expander inputs with pull-ups
+	gpio_mode_setup(IN_PORT, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, IN_PINS);
+	
+	//expander outputs
+	gpio_mode_setup(OUT_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, OUT_PINS);
+	gpio_clear(OUT_PORT, OUT_PINS);
+	
 }
 
 
 //derived from examples at https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/f1/other/i2c_stts75_sensor/i2c_stts75_sensor.c and https://github.com/libopencm3/libopencm3-examples/blob/15637e291b8ca228e35d5f657ed15f3b8958fa0c/examples/stm32/f3/stm32f3-discovery/i2c/i2c.c
+//additional example of i2c slave mode: https://github.com/amitesh-singh/i2c-slave-stm32f1/blob/master/main.cpp#L49
+
 static void i2c_setup(void)
 {
 	//rcc_periph_reset_pulse(RST_I2C1);
@@ -50,24 +79,38 @@ static void i2c_setup(void)
 	i2c_set_digital_filter(I2C1, 0);
 	i2c_set_speed(I2C1, i2c_speed_fm_400k, 16);
 	i2c_enable_stretching(I2C1);
-	i2c_enable_interrupt(I2C1, I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_STOPIE | I2C_CR1_NACKIE); //enable interrupts. Also available: I2C_CR1_ERRIE, etc
+	i2c_enable_interrupt(I2C1, I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_TXIE
+		| I2C_CR1_STOPIE | I2C_CR1_NACKIE); //enable interrupts. Also available: I2C_CR1_ERRIE, etc
 	nvic_enable_irq(NVIC_I2C1_IRQ);
 	i2c_peripheral_enable(I2C1);
 }
 
 void i2c1_isr(void){
 	uint32_t isr_status = I2C_ISR(I2C1);
+	
+	//address match
 	if(isr_status & I2C_ISR_ADDR) {
-		//address match, clear flag to release clock
-		I2C_ICR(I2C1) = I2C_ICR_ADDRCF;
+		is_read_transfer = !!(isr_status & I2C_ISR_DIR_READ);
+		I2C_ICR(I2C1) = I2C_ICR_ADDRCF; //clear flag to release clock stretching
 	}
+	
+	//read from master
+	if(isr_status & I2C_ISR_TXIS) {
+		I2C_TXDR(I2C1) = inbyte;
+	}
+	
+	//byte received from master
 	if(isr_status & I2C_ISR_RXNE){
-		//byte received, reading RXDR clears flag automatically
-		inbyte = i2c_get_data(I2C1);
+		//reading RXDR clears flag automatically
+		outbyte = I2C_RXDR(I2C1); //equivalent to i2c_get_data(I2C1);
 	}
 	if (isr_status & I2C_ISR_STOPF){ //clear the stop, mark data ready
 		I2C_ICR(I2C1) = I2C_ICR_STOPCF;
-		byte_received = true;
+		if(is_read_transfer){
+			new_read = true;
+		} else {
+			new_write = true;
+		}
 	}
 	
 	if (isr_status & I2C_ISR_NACKF) { //clear a nack flag
@@ -80,23 +123,24 @@ void i2c1_isr(void){
 int main(void) {
 	clock_setup();
 	gpio_setup();
-	//TODOTODOTODO//////: setup NVIC interrupt handling for i2c
-	//follow this example https://github.com/amitesh-singh/i2c-slave-stm32f1/blob/master/main.cpp#L49
-	//nvic docuemntation: https://libopencm3.org/docs/latest/stm32l0/html/group__CM3__nvic__defines.html#gae5fdef5fd0dc9db35df8e84715fe8179
 	i2c_setup();
 	
 	
 	while(1) {
-		/* wait a little bit */
-		for (int i = 0; i < BLINK_DELAY; i++) {
-			__asm__("nop");
+		//continuously sample inputs for fast response
+		inbyte = (gpio_get(IN_PORT, IN_PINS) >> IN_PORT_SHIFT) & IN_MASK;
+		
+		if (new_write){
+			new_write = false;
+			uint16_t new_out = (outbyte & OUT_MASK) << OUT_PORT_SHIFT;
+			gpio_port_write(OUT_PORT, (gpio_port_read(OUT_PORT) & ~OUT_PINS) | new_out);
+			gpio_toggle(LED_PORT, LED_PIN);
 		}
-		if (byte_received){
-			gpio_toggle(LED);
-			byte_received = false;
+		
+		if (new_read) {
+			new_read = false;
+			//inbyte was already sent in TXIS, no action needed here
+			gpio_toggle(LED_PORT, LED_PIN);
 		}
 	}
-	//uint32_t rev = 0xaabbccdd;
-	//rev = rev_bytes(rev);
-	//return my_func(rev);
 }
